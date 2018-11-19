cd275c931d8d5c913e6577fab88dc6e18d234244
Jing Zhang <jiz346@g.harvard.edu>
2018-11-19 05:55:30 -0800

    finished filter

--- vanilla/js/gallery.js
+++ vanilla/js/gallery.js
@@ -1,1 +1,31 @@
-var Shuffle = window.Shuffle;
+$(document).ready(() => {
+
+  var $grid = $('.gallery-items').isotope({
+    // options
+    itemSelector: '.gallery-item',
+    layoutMode: 'masonry',
+    masonry: {
+      rowWidth: 200,
+      columnWidth: 200
+    }
+  });
+
+  $('#all-button').click(() => {
+    $grid.isotope({
+      filter: '*'
+    });
+  });
+
+  $('#landscape-button').click(() => {
+    $grid.isotope({
+      filter: '.landscape'
+    });
+  });
+
+  $('#wildlife-button').click(() => {
+    $grid.isotope({
+      filter: '.wildlife'
+    });
+  });
+
+});
